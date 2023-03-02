a16093781dc093baddc3ffdd85eafe7d2577f136
Adimora Uju Louisa <adimorauju@gmail.com>
2023-03-02 15:11:24 +0000

    Modify the employ data  and upadate function

--- run.py
+++ run.py
@@ -22,59 +22,62 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 # Connect to the Google Sheets API
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
  #Open the spreadsheet
-SHEET = GSPREAD_CLIENT.open("simple_payrolls")
+SHEET = GSPREAD_CLIENT.open("simple_payrolls").sheet1
+#sheet = client.open('Employee Data').sheet1
+
 
 def employee_data():
     global name
     """
-    Get the employee details vie the Terminal
+    Get the employee details vie the Terminal and add the
+     employee data to the google spreadheet
     """
-    answer = input("Do you want to enter employee data? (yes/no/end): ")
-
-    print("Please enter Employee Details to update the Employees record \n")
-
-    id =  input('Enter the ID number of Employee  \n ')
-
-    name =  input('Enter the name of  Employee  \n ')
-
-    age = int(input('Enter  Employee Age \n '))
-
-    department =  input('Enter  the  Employee Department \n ')
-
-    salary = float(input('Enter  employee  Basic Salary(gross income) \n'))
-
-    data_details = [id, name, age, department, salary]
-
-    print(data_details)
-
     
-#employee_data()
+    #answer = input("Do you want to enter employee data? (yes/no/end): ")
+    employees = []
+    while True:
+        id = input("Enter employee id: ")
+        if not id.isalnum():
+            print("Invalid id. Please enter a valid id containing only letters.")
+            continue
+        name = input("Enter employee name: ")
+        if not name.isalpha():
+            print("Invalid name. Please enter a valid name containing only letters.")
+            continue
+        age = input("Enter employee age: ")
+        if not age.isdigit() or int(age) < 18 or int(age) > 100:
+            print("Invalid age. Please enter a valid age between 18 and 100.")
+            continue
+        department = input("Enter employee department: ")
+        if not department.isalpha():
+            print("Invalid department. Please enter a valid position containing only letters.")
+            continue
+        salary = input("Enter employee Basic Salary: ")
+        if not salary.isdigit():
+            print("Invalid salary. Please enter a valid salary containing only numbers.")
+            continue
+        employees.append({"id": id,"name": name, "age": age, "department": department})
 
-def update_employee_record(data):
-    """
-    update the emplyee record worksheet
-    """
-    print("updating  employee record... \n")
+        # add the employ detail to the spreed sheet
+        print("updating  employee record... \n")
+        SHEET.append_row([id, name, age, department, salary])
+        print("Employee Records  update sucessfull \n")
+        another_employee = input("Do you want to enter another employee? (yes/no): ")
+        if another_employee.lower() == "no":
+            break
+    return employees
 
-    employee_worksheet = SHEET.worksheet("employees")
-    
-    employee_worksheet.append_row(data_details)
-
-    print("Employee Records  update sucessfull \n")
-
-data_details= employee_data()
-
-
-#update_employee_record(data_details)
-#cal_over_time()
-
-
-    
+employees = employee_data()
+print(employees)
+ 
 
 def cal_over_time():
     """
     this is to calcualte the over time per week
     """
+    
+    
+    print('Enter the name of the employee to ge Net salary for the week')
     # assume the regular work hours and maximum hours per day
     
     employee_regular_hours = 40
@@ -83,6 +86,7 @@ def cal_over_time():
 
    # # Define the regular work hours and maximum hours per day
     global name
+    
     name =  input('enter  employee Name \n ')
     #employee_regular_hours = 40
     #max_hours_per_day = 8
@@ -115,7 +119,7 @@ def cal_over_time():
     print("Overtime hours:", over_time)
     #print("Overtime pay:", overtime_pay)
 
-#cal_over_time()
+cal_over_time()
 
 
 def cal_net_pay():
@@ -129,27 +133,8 @@ def cal_net_pay():
 
     print(net_pay)
 
-#cal_net_pay()
+cal_net_pay()
 
-while True:
-    # ask user if they want to enter employee data
-    answer = input("Do you want to enter employee data? (yes/no/end): ")
-
-    if answer.lower() == "yes":
-        # call employee_data() function
-        employee_data()
-        update_employee_record(data_details)
-    elif answer.lower() == "no":
-        # call overtime() and netpay() functions
-        cal_over_time()
-        cal_net_pay()
-
-    elif answer.lower() == "end":
-        # exit the program
-        break
-    else:
-        # if the user enters an invalid answer, prompt them again
-        print("Invalid input. Please enter 'yes', 'no', or 'end'.")
 
 
 
@@ -171,6 +156,27 @@ while True:
 #update_employee_record(data_details)
 
 
-    
 
+
+
+
+# define a function to add employee data to the spreadsheet
+#def add_employee_data():
+ #   name = input('Enter employee name: ')
+  #  age = input('Enter employee age: ')
+   # position = input('Enter employee position: ')
+    #salary = input('Enter employee salary: ')
     
+    # add the employee data to the spreadsheet
+    #SHEET.append_row([name, age, position, salary])
+    
+    # ask the user if they want to continue adding data
+    #answer = input('Do you want to add more data? (y/n) ')
+    #if answer == 'y':
+        #add_employee_data()
+    #else:
+        ## call another function or exit the program here
+        #print('Program completed.')
+
+# call the function to add employee data
+#add_employee_data()
